fe3c0136ed219bee47da0f16abeacb054863660a
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-10 19:23:16 +0300

    Initial commit

---
+++ LeetCodeSolutions/0001-Two Sum/Solution.cs
@@ -0,0 +1,34 @@
+﻿namespace LeetCodeSolutions._0001_Two_Sum
+{
+    public class Solution
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int currentNumber, newTarget;
+            int[] result = new int[2];
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                currentNumber = nums[i];
+                newTarget = target - currentNumber;
+
+                for (int j = i+1; j < nums.Length; j++)
+                {
+                    if (nums[j] == newTarget)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
+                }
+            }
+
+            return result;
+        }
+        
+        
+        
+        
+        
+    }
+}
---
+++ LeetCodeSolutions/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("LeetCodeSolutions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LeetCodeSolutions")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3B3229C9-1B44-4CEA-9C05-548AB3625019")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

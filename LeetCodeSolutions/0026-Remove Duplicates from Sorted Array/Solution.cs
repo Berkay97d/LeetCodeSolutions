70656ef7924bc7ef02f79c253b99bf888be53160
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-13 22:46:16 +0300

    0026-Remove-Duplicates

---
+++ LeetCodeSolutions/0026-Remove Duplicates from Sorted Array/Solution.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions._0026_Remove_Duplicates_from_Sorted_Array
+{
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            int counter = 0;
+            
+            for (int i = 0; i < nums.Length/2; i++)
+            {
+                if (nums[i] == nums[i+1])
+                {
+                    nums[i] = -101;
+                }
+
+                if (nums[nums.Length - (i+1)] == nums[nums.Length - (i+2)])
+                {
+                    nums[nums.Length - (i + 1)] = -101;
+                }
+            }
+
+            foreach (var num in nums)
+            {
+                if (num != -101)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}

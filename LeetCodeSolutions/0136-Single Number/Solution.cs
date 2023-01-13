99a7a7b10f98a16058a1342c66e27b440f39e405
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-13 22:46:28 +0300

    0136-Single Number

---
+++ LeetCodeSolutions/0136-Single Number/Solution.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCodeSolutions._0136_Single_Number
+{
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int index = 0;
+            int uniqueNum;     
+
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                uniqueNum = nums[index];
+                
+                if (index == i)
+                {
+                    continue;
+                }
+                
+                if (uniqueNum == nums[i])
+                {
+                    index++;
+                    i = -1;
+                }
+            }
+
+            return nums[index]; 
+        }
+    }
+}

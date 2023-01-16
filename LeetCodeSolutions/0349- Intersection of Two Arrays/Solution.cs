d21d41f79ce6058d4f4fc05f3a9f743b2e6af8f9
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-16 23:39:49 +0300

    349-Intersection of Two Array

---
+++ LeetCodeSolutions/0349- Intersection of Two Arrays/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeSolutions._0349__Intersection_of_Two_Arrays
+{
+    public class Solution
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j] && !result.Contains(nums1[i]))
+                    {
+                        result.Add(nums1[i]);
+                    }
+                }
+            }
+            
+            int[] finalResults = new int[result.Count];
+
+            for (int i = 0; i < result.Count; i++)
+            {
+                finalResults[i] = result[i];
+            }
+
+            return finalResults;
+        }
+    }
+}

c5f2df75d3424958589342f5e61cccfc5ce9ec3c
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-19 13:26:49 +0300

    Create Solution.cs

---
+++ LeetCodeSolutions/0121-Best Time to Buy and Sell Stock/Solution.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolutions._0121_Best_Time_to_Buy_and_Sell_Stock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)      //TODO Time Limit Exceeded
+        {
+            int bestProfit = 0;
+            
+            
+            for (int i = 0; i < prices.Length - 1; i++)
+            {
+                for (int j = i+1; j < prices.Length; j++)
+                {
+                    if (prices[j] - prices[i] > bestProfit)
+                    {
+                        bestProfit = prices[j] - prices[i];
+                    }
+                }
+            }
+
+            return bestProfit;
+
+
+
+        }
+        
+    }
+}

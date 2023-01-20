8e8e9d196cebb31878750f439e2aa683cff56512
Berkay97d <94765038+Berkay97d@users.noreply.github.com>
2023-01-20 21:12:46 +0300

    Create Solution.cs

---
+++ LeetCodeSolutions/0066-Plus One/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCodeSolutions._0066_Plus_One
+{
+    public class Solution
+    {
+        public int[] PlusOne(int[] digits) 
+        {
+            if (digits[digits.Length - 1] != 9)
+            {
+                digits[digits.Length - 1] += 1;
+                return digits;
+            }
+            
+            //TODO son eleman 9 ise, devretme mantığını ekleyelim
+            return digits;
+        }
+    }
+}

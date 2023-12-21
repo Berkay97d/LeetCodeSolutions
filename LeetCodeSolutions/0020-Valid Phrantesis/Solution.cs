c9ca94064a52584153dcbf17d42516f9b32dc223
Berkay Dindar <berkay97d@gmail.com>
2023-12-21 11:40:20 +0300

    0020-Valid Phrantesis Full Solution

---
+++ LeetCodeSolutions/0020-Valid Phrantesis/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCodeSolutions._0020_Valid_Phrantesis
+{
+    public class Solution
+    {
+
+        public bool IsValid(string s)   
+        {
+            Dictionary<char, char> dictionary = new Dictionary<char, char>()
+            {
+                { ')', '('},
+                { ']', '['},
+                { '}', '{'}
+            };
+            
+            Stack<char> openerStack = new Stack<char>();
+            
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dictionary.TryGetValue(s[i], out char value))
+                {
+                    if (openerStack.Count == 0)
+                    {
+                        return false;
+                    }
+                    
+                    if (value != openerStack.Pop())
+                    {
+                        return false;   
+                    }
+                }
+                
+                else
+                {
+                    openerStack.Push(s[i]); 
+                }    
+            }
+            
+            return openerStack.Count == 0;
+        }
+    }
+}
+

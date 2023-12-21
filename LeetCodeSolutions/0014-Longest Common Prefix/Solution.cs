77a67662b8ecfb1f471a0e6d12b4ba012a6c1201
Berkay Dindar <berkay97d@gmail.com>
2023-12-21 10:53:20 +0300

    0014-Longest Common Prefix Full Solution

---
+++ LeetCodeSolutions/0014-Longest Common Prefix/Solution.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeSolutions._0014_Longest_Common_Prefix
+{
+    public class Solution
+    {
+        public static void Main()
+        {
+            
+        }
+
+        private string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0) { return ""; }
+            if (strs.Length == 1) { return strs[0]; }
+
+            string shortestStr = strs[0];
+            
+            for (var i = 0; i < strs.Length; i++)
+            {
+                var str = strs[i];
+                
+                if (str.Length < shortestStr.Length)
+                {
+                    shortestStr = str;
+                }
+            }
+            
+            
+            for (int i = 0; i < shortestStr.Length; i++)   
+            {
+                for (int j = 0; j < strs.Length; j++)
+                {
+                    if (shortestStr[i] != strs[j][i])
+                    {
+                        return shortestStr.Substring(0, i);
+                    }
+                }
+            }
+            
+            return shortestStr;
+        }
+    }
+}
--- LeetCodeSolutions/0026-Remove Duplicates from Sorted Array/Solution.cs
+++ LeetCodeSolutions/0026-Remove Duplicates from Sorted Array/Solution.cs
@@ -6,11 +6,6 @@ namespace LeetCodeSolutions._0026_Remove_Duplicates_from_Sorted_Array
 {
     public class Solution
     {
-        private static void Main()
-        {
-            int[] nums = new[] {0,0,0,0,0,0,0,0,0,0,1, 2, 3, 4, 5, 6,6};
-        }
-
         
         private static int RemoveDuplicates(int[] nums)         //BRUT-FORCE USED, IMPROVE IT !!!
         {

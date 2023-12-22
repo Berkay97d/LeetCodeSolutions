57cd0b67204583462c98396709c9707c4f4cb4d5
Berkay Dindar <berkay97d@gmail.com>
2023-12-22 11:26:36 +0300

    0027-Remove Element Full Solution

---
+++ LeetCodeSolutions/0027-Remove Element/Solution.cs
@@ -0,0 +1,38 @@
+﻿namespace LeetCodeSolutions._0027_Remove_Element
+{
+    public class Solution
+    {
+        public int RemoveElement(int[] nums, int val)
+        {
+            var countOfNonVolNumber = 0;
+            
+            foreach (var num in nums)
+            {
+                if (num != val)
+                {
+                    countOfNonVolNumber++;
+                }
+            }
+            
+            int[] nonVolNumberIndexes = new int[countOfNonVolNumber];
+            var counter = 0;
+            
+            for (var i = 0; i < nums.Length; i++)
+            {
+                var num = nums[i];
+                if (num != val)
+                {
+                    nonVolNumberIndexes[counter] = nums[i];
+                    counter++;
+                }
+            }
+
+            for (int i = 0; i < nonVolNumberIndexes.Length; i++)
+            {
+                nums[i] = nonVolNumberIndexes[i];
+            }
+
+            return countOfNonVolNumber;
+        }
+    }
+}
